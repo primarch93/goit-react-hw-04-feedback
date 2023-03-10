3186c52f3717ec346c044131737900ac16b7a9aa
primarch93 <sugonegor@gmail.com>
2023-03-10 21:45:41 +0300

    fixed

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NotificationWrapp } from '../Notification/Notification.styled';
 
-class Notification extends Component {
-  render() {
-    return (
-      <NotificationWrapp className="notificationBlock">
-        <p className="notificationText">There is no feedback</p>
-      </NotificationWrapp>
-    );
-  }
-}
+const Notification = () => {
+return (
+<NotificationWrapp className="notificationBlock">
+<p className="notificationText">There is no feedback</p>
+</NotificationWrapp>
+);
+};
 
 export default Notification;
